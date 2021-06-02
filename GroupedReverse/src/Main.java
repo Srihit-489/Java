f6816bb314640c7ce41e388bdd2901861c537679
Srihit-489 <71779278+Srihit-489@users.noreply.github.com>
2021-06-02 22:55:48 +0530

    Add files via upload

---
+++ GroupedReverse/src/Main.java
@@ -0,0 +1,131 @@
+
+
+class Main {
+    public static void main(String[] args) {
+        Linkedlist mylist = new Linkedlist();
+        mylist.insertHead(9);
+        mylist.insertHead(8);
+        mylist.insertHead(7);
+        mylist.insertHead(6);
+        mylist.insertHead(5);
+        mylist.printList();
+       // System.out.println();
+        Linkedlist reversed = new Linkedlist();
+       // printReverse(mylist.getHead());
+        mylist.reverseList();
+        mylist.printList();
+        mylist.reverInRange(2,4);
+    }
+    public static void printReverse(Linkedlist.Node node){
+        if(node == null){
+            return;
+        }
+        printReverse(node.next);
+        System.out.print(node.data + " ");
+    }
+
+
+
+}
+
+class Linkedlist{
+    Node head;
+    public Linkedlist(){
+        this.head = null;
+    }
+    public Node getHead(){
+        return this.head;
+    }
+    public void reverseList(){
+        Node current = head;
+        Node Next = null;
+        Node prev = null;
+        while(current!= null){
+            Next = current.next;
+            current.next = prev;
+            prev = current;
+            current = Next;
+        }
+        head = prev;
+    }
+
+    public void reverseList(Node head,int limit){
+        Node current = head;
+        Node Next = null;
+        Node prev = null;
+        while(current!= null){
+            Next = current.next;
+            current.next = prev;
+            prev = current;
+            current = Next;
+        }
+        head = prev;
+    }
+
+    public void reverseInRange(int lower,int higher){
+        Node temp = this.head;
+        while(lower != 0){
+            temp = temp.next;
+            lower = lower-1;
+        }
+        reverseList(temp,higher);
+    }
+
+    public void printList(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public void insertHead(int data){
+        Node newnode = new Node(data);
+        if(head == null){
+            this.head = newnode;
+            return;
+        }
+        newnode.next = head;
+        head = newnode;
+        return;
+    }
+    public void insertHead(Node newnode){
+        if(head == null){
+            this.head = newnode;
+            return;
+        }
+        newnode.next = head;
+        head = newnode;
+        return;
+    }
+    public void deleteHead(){
+        head = head.next;
+    }
+
+    public Node searchNode(int data){
+        Node temp = head;
+        while(temp != null){
+            if(temp.data == data){
+                return temp;
+            }
+        }
+        return null;
+    }
+
+    public void insertAfter(Node nearNode,int data){
+        Node newnode = new Node(data);
+        newnode.next = nearNode.next;
+        nearNode.next = newnode;
+    }
+
+
+    class Node{
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
